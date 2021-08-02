93a4e7868a9975a12966e465be859187b27a204b
ian k <kickthechair@gmail.com>
2021-08-02 16:26:08 +0100

    step 7 complete - added subtract money code & test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,19 @@
 class Oystercard
 
   attr_reader :balance
+
   MAX_AMOUNT = 90
+
   def initialize()
     @balance = 0
   end 
+
   def top_up(add)
     raise "Exceeds the limit #{MAX_AMOUNT}" if (@balance + add ) > MAX_AMOUNT
     @balance += add
   end
 
+  def deduct(subtract)
+    @balance -= subtract
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,15 +9,28 @@ describe Oystercard do
   it 'has a balance' do
     expect(subject.balance).to eq(0)
   end
+
   it "responds to top_up" do
     expect(subject).to respond_to(:top_up).with(1).argument
   end
+
   it "adds money to the oystercard" do
-    expect(subject.top_up(5)).to eq(5)
+    # expect(subject.top_up(5)).to eq(5)
+    expect{subject.top_up(5)}.to change{subject.balance}.by 5
   end
+
   it "raises error when money exceeds £90 " do
     max_balance = Oystercard::MAX_AMOUNT
     subject.top_up(max_balance)
     expect {subject.top_up(1)}.to raise_error("Exceeds the limit #{max_balance}")
   end
+
+  it 'responds to deduct' do
+    expect(subject).to respond_to(:deduct).with(1).argument
+  end
+
+  it "subtracts money from the oystercard" do
+    expect{subject.deduct(5)}.to change{subject.balance}.by -5
+  end
+
 end
