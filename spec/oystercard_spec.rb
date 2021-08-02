0d10a060ee3681a6218e41b3f37f1f4f38e209e4
ian k <kickthechair@gmail.com>
2021-08-02 15:11:06 +0100

    Added initial balance check not to be 0 and created initialize method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,15 @@
+require 'oystercard'
 
-describe oystercard do
+describe Oystercard do
 
+  let(:card) { Oystercard.new(5)}
+
+  it 'responds to balance' do
+    expect(subject).to respond_to(:balance)
+  end
+
+  it 'has a balance' do
+    expect(card.balance).not_to be(0)
+  end
+  
 end
