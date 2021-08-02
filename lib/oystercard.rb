7c25389ec9fb853c158e9ba07bb4c9cd513101f3
Suhani Zaman <suhanibadiz9@gmail.com>
2021-08-02 19:05:24 +0400

    done till step 6 code and test for adding and maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,13 @@
 class Oystercard
 
   attr_reader :balance
-
-  def initialize(balance = 0)
-    @balance = balance 
+  MAX_AMOUNT = 90
+  def initialize()
+    @balance = 0
   end 
+  def top_up(add)
+    raise "Exceeds the limit #{MAX_AMOUNT}" if (@balance + add ) > MAX_AMOUNT
+    @balance += add
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,14 +2,22 @@ require 'oystercard'
 
 describe Oystercard do
 
-  let(:card) { Oystercard.new(5)}
-
   it 'responds to balance' do
     expect(subject).to respond_to(:balance)
   end
 
   it 'has a balance' do
-    expect(card.balance).not_to be(0)
+    expect(subject.balance).to eq(0)
   end
-  
+  it "responds to top_up" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+  it "adds money to the oystercard" do
+    expect(subject.top_up(5)).to eq(5)
+  end
+  it "raises error when money exceeds £90 " do
+    max_balance = Oystercard::MAX_AMOUNT
+    subject.top_up(max_balance)
+    expect {subject.top_up(1)}.to raise_error("Exceeds the limit #{max_balance}")
+  end
 end
